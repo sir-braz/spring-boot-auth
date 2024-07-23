c6d0d09d929d903a804f4dc43e4c7e4ba74083fa
sir-braz <mrbrazdev@gmail.com>
2024-07-22 23:12:42 -0300

    Add POST /api/auth/register endpoint to register new users

--- backend/src/main/java/com/example/backend/controller/AuthController.java
+++ backend/src/main/java/com/example/backend/controller/AuthController.java
@@ -1,4 +1,16 @@
 package com.example.backend.controller;
 
+import com.example.backend.entity.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AuthController {
+
+    @PostMapping("/api/auth/register")
+    public ResponseEntity<Void> registerUser(@RequestBody User user){
+
+    }
 }
