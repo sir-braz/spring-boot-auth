c98bc21314da871fb620678d4f9634ef05b1cf65
sir-braz <mrbrazdev@gmail.com>
2024-07-22 23:30:56 -0300

    Add PUT /api/roles/{id} endpoint to update roles by id

--- backend/src/main/java/com/example/backend/controller/RoleController.java
+++ backend/src/main/java/com/example/backend/controller/RoleController.java
@@ -4,6 +4,7 @@ package com.example.backend.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,6 +22,11 @@ public class RoleController {
 
     @GetMapping("/api/roles/{id}")
     public ResponseEntity<Void> listRoleById(){
+
+    }
+
+    @PutMapping("/api/roles/{id}")
+    public ResponseEntity<Void> updateRoleWithId(){
 
     }
 
