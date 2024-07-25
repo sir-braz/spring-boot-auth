9c93090ec7b9266b72d8a2c042e3c37757b739fe
sir-braz <mrbrazdev@gmail.com>
2024-07-25 00:24:04 -0300

    Add AuthService interface with methods for user creation, sign-in, and token update

--- backend/src/main/java/com/example/backend/service/AuthService.java
+++ backend/src/main/java/com/example/backend/service/AuthService.java
@@ -1,4 +1,12 @@
 package com.example.backend.service;
 
+import com.example.backend.entity.User;
+import org.springframework.http.ResponseEntity;
+
 public interface AuthService {
+    ResponseEntity<Void> createNewUser(User user);
+
+    ResponseEntity<Void> siginUser();
+
+    ResponseEntity<Void> updateToken();
 }
