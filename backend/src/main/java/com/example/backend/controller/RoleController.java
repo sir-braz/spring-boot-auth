5d44ee39ec3201a0e3f7dd50eef3454d84af34ed
sir-braz <mrbrazdev@gmail.com>
2024-07-24 21:56:03 -0300

    Add RoleController with dependency injection for RoleService

--- backend/src/main/java/com/example/backend/controller/RoleController.java
+++ backend/src/main/java/com/example/backend/controller/RoleController.java
@@ -1,11 +1,21 @@
 package com.example.backend.controller;
 
 
+import com.example.backend.service.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class RoleController {
+
+    private final RoleService roleService;
+
+    @Autowired
+    public RoleController(RoleService roleService) {
+        this.roleService = roleService;
+    }
+
 
     @PostMapping("/api/roles")
     public ResponseEntity<Void> createRole(){
