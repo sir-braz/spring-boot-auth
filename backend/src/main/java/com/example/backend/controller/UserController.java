a2a40afd05a5ee291dcd4973ad8f1a2df9daa220
sir-braz <mrbrazdev@gmail.com>
2024-07-25 14:50:49 -0300

    Fix listAllUsers method to correctly return ResponseEntity with list of users

--- backend/src/main/java/com/example/backend/controller/UserController.java
+++ backend/src/main/java/com/example/backend/controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.backend.controller;
 import com.example.backend.entity.User;
 import com.example.backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,8 +21,13 @@ public class UserController {
 
 
     @GetMapping("/api/users")
-    public ResponseEntity<List<User>> listAllUsers(){
-        return userService.getAllUsers();
+    public ResponseEntity<List<User>> listAllUsers() {
+        try {
+            List<User> users = (List<User>) userService.getAllUsers();
+            return new ResponseEntity<>(users, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @GetMapping("/api/user/{id}")
