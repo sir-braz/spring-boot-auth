fb838444cac3910bd86ce7a8b430f85427ee197f
sir-braz <mrbrazdev@gmail.com>
2024-07-27 21:29:08 -0300

    Add DTO classes for authentication: AuthRequest, AuthResponse, and RefreshTokenRequest

--- backend/src/main/java/com/example/backend/service/AuthService.java
+++ backend/src/main/java/com/example/backend/service/AuthService.java
@@ -1,10 +1,9 @@
 package com.example.backend.service;
 
-import com.example.backend.entity.User;
 import org.springframework.http.ResponseEntity;
 
 public interface AuthService {
-    ResponseEntity<Void> createNewUser(User user);
+    ResponseEntity<Void> createNewUser(AuthRequest user);
 
     ResponseEntity<Void> signinUser();
 
