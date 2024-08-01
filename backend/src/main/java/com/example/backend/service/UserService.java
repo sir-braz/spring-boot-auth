ceffbb1e098e794f3dcff6391cfddf38b74ad4fd
sir-braz <mrbrazdev@gmail.com>
2024-08-01 19:10:40 -0300

    Add UserService interface with methods to find all users, find user by ID, and update user

--- backend/src/main/java/com/example/backend/service/UserService.java
+++ backend/src/main/java/com/example/backend/service/UserService.java
@@ -1,4 +1,14 @@
 package com.example.backend.service;
 
-public class UserService {
+import com.example.backend.entity.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    List<User> findAll();
+
+    User findById(Long id);
+
+    void updateUser(Long id, User updateUser);
 }
