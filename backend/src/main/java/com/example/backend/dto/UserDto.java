51334bd2f31ad13970290bf0ecd1d59599940535
sir-braz <mrbrazdev@gmail.com>
2024-08-09 15:06:54 -0500

    Add UserDto class for user data transfer

--- backend/src/main/java/com/example/backend/dto/UserDto.java
+++ backend/src/main/java/com/example/backend/dto/UserDto.java
@@ -1,4 +1,49 @@
 package com.example.backend.dto;
 
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
 public class UserDto {
+
+    @NotEmpty(message = "Username cannot be empty")
+    @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
+    private String username;
+
+    @NotEmpty(message = "Password cannot be empty")
+    @Size(min = 6, message = "Password must be at least 6 characters")
+    private String password;
+
+    public UserDto() {
+    }
+
+    public UserDto(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    // Getters e Setters
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "UserDto{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
