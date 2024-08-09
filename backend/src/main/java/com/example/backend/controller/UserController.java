02f83a1a901018660c608609a9d88448087a0049
sir-braz <mrbrazdev@gmail.com>
2024-08-09 15:14:00 -0500

    Implement user registration endpoint in UserController with UserService integration

--- backend/src/main/java/com/example/backend/controller/UserController.java
+++ backend/src/main/java/com/example/backend/controller/UserController.java
@@ -1,7 +1,11 @@
 package com.example.backend.controller;
 
+import com.example.backend.dto.UserDto;
 import com.example.backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +16,11 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody UserDto userDto){
+        userService.saveUser(userDto);
+        return ResponseEntity.ok("User Registered successfully");
+    }
 
 }
