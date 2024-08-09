dd730d6cb1e97ad314e9bed6b7fa8a7de771cee3
sir-braz <mrbrazdev@gmail.com>
2024-08-09 15:09:49 -0500

    Create UserController class for user management endpoints

--- backend/src/main/java/com/example/backend/controller/UserController.java
+++ backend/src/main/java/com/example/backend/controller/UserController.java
@@ -1,4 +1,13 @@
 package com.example.backend.controller;
 
+import com.example.backend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+
+
 }
