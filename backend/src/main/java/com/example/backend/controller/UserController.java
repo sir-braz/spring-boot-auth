4797eca00ae8745d4e8f39bf2b274270eb5e5d1c
sir-braz <mrbrazdev@gmail.com>
2024-08-09 18:29:42 -0500

    Implement basic login endpoint in UserController with authentication

--- backend/src/main/java/com/example/backend/controller/UserController.java
+++ backend/src/main/java/com/example/backend/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.example.backend.controller;
 
 import com.example.backend.dto.UserDto;
+import com.example.backend.entity.User;
 import com.example.backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,6 +24,16 @@ public class UserController {
     public ResponseEntity<String> register(@RequestBody UserDto userDto){
         userService.saveUser(userDto);
         return ResponseEntity.ok("User Registered successfully");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody UserDto userDto){
+        boolean isAuthenticate = userService.authenticateUser(userDto);
+        if(isAuthenticate){
+            return ResponseEntity.ok("Login sucessful");
+        }else{
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
+        }
     }
 
 }
